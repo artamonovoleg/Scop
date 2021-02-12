5b24cd0ad032e065704599a4902adfb7fe86f63e
artamonovoleg <artamonovav2003@gmail.com>
2021-02-12 15:03:29 +0300

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -103,144 +103,6 @@ class VertexBuffersLayout
 
 ImGuiIO* imgio = nullptr;
 
-//int main()
-//{
-//    GLFWwindow* window = nullptr;
-//    float deltaTime = 0.0f;
-//    float lastFrame = 0.0f;
-//    OBJLoader loader;
-////--------------------------------------Setup part--------------------------------------------------------------------//
-//    // glfw: initialize and configure
-//    glfwInit();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//    // glfw window creation
-//    window = glfwCreateWindow(800, 600, "Scop", nullptr, nullptr);
-//    glfwMakeContextCurrent(window);
-//
-//    // tell GLFW to capture our mouse
-////    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-//    glfwSetCursorPosCallback(window, mouse_callback);
-//    // glad: load all OpenGL function pointers
-//    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-//        std::cerr << "Failed to initialize GLAD" << std::endl;
-//
-//    glViewport(0, 0, 800, 600);
-////------------------------------------------ImGUI---------------------------------------------------------------------//
-//    ImGui::CreateContext();
-//    ImGuiIO& io = ImGui::GetIO(); (void)io;
-//    imgio = &io;
-//    ImGui::StyleColorsDark();
-//    ImGui_ImplGlfw_InitForOpenGL(window, true);
-//    ImGui_ImplOpenGL3_Init((char *)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
-////-----------------------------------Create render objects------------------------------------------------------------//
-//
-//    /* TODO:
-//     * 1. Think about buffer abstraction
-//     * 2. Layout can take only amount and type
-//    */
-//
-//    // load model
-//    Mesh mesh = loader.LoadFromFile("res/objs/diablo.obj");
-//    // load model to buffers
-//    unsigned int ebo;
-//    VertexArray va;
-//    va.Bind();
-//    VertexBuffer<Vertex> vb(mesh.m_Vertices.data(), mesh.m_Vertices.size());
-//    vb.Bind();
-//
-//    glGenBuffers(1, &ebo);
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.m_Indices.size() * sizeof(unsigned int), mesh.m_Indices.data(), GL_STATIC_DRAW);
-//
-//    VertexBuffersLayout layout;
-//    layout.Push<float>(3, 8, 0);
-//    layout.Push<float>(2, 8, 3);
-//    layout.Push<float>(3, 8, 5);
-//
-//
-//    glEnable(GL_DEPTH_TEST);
-//
-//    {
-//        // class objects need to call their destructor earlier then glfw terminates
-//        Shader shader("res/shaders/vshader.vert", "res/shaders/fshader.frag");
-//        Texture texture("res/textures/diablo3_pose_diffuse.tga");
-//
-////[*}--------------------------------Application loop---------------------------------------------------------------[*]//
-//        while (!glfwWindowShouldClose(window))
-//        {
-//            static float lightColorArray[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-//            glm::vec3 lightColor(lightColorArray[0], lightColorArray[1], lightColorArray[2]);
-//            glm::vec3 diffuseColor = lightColor * glm::vec3(0.9f);
-//            glm::vec3 ambientColor = diffuseColor * glm::vec3(0.9f);
-//
-//            // per-frame time logic
-//            auto currentFrame = static_cast<float>(glfwGetTime());
-//            deltaTime = currentFrame - lastFrame;
-//            lastFrame = currentFrame;
-////---------------------------------Shader update-----------------------------------------------------------------------//
-//            shader.SetVec3("viewPos", camera.Position);
-//
-//            shader.SetVec3("light.ambient", ambientColor);
-//            shader.SetVec3("light.diffuse", diffuseColor);
-//            shader.SetVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
-//
-//            shader.SetVec3("material.ambient", glm::vec3(1.0f, 1.0f, 1.0f));
-//            shader.SetVec3("material.diffuse", glm::vec3(0.9f, 0.9f, 0.9f));
-//            shader.SetVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
-//            shader.SetFloat("material.shininess", 32.0f);
-//
-//            shader.SetVec3("light.position", 0.0f, 1.0f, 0.0f);
-//
-//            // pass projection matrix to shader (note that in this case it could change every frame)
-//            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)800 / (float)600, 0.1f, 100.0f);
-//            shader.SetMat4("projection", projection);
-//            // camera/view transformation
-//            glm::mat4 view = camera.GetViewMatrix();
-//            shader.SetMat4("view", view);
-//
-//            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix
-//            model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-//            float angle = 0.0f;
-//            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-//            shader.SetMat4("model", model);
-////--------------------------------Draw part---------------------------------------------------------------------------//
-//            // wipe the drawing surface clear
-//            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//            glClearColor(0.5, 0.5, 0.5, 0.5);
-//            texture.Bind();
-//            shader.Bind();
-////            glBindVertexArray(vao);
-//            va.Bind();
-//            glDrawElements(GL_TRIANGLES, mesh.m_Indices.size(), GL_UNSIGNED_INT, nullptr);
-//
-//            ImGui_ImplOpenGL3_NewFrame();
-//            ImGui_ImplGlfw_NewFrame();
-//            ImGui::NewFrame();
-//
-//            // render your GUI
-//            ImGui::Begin("Gui");
-//            ImGui::ColorEdit3("Light", lightColorArray);
-//            ImGui::End();
-//
-//            // Rendering
-//            ImGui::Render();
-//            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-////-----------------------------Every frame logic----------------------------------------------------------------------//
-//            glfwSwapBuffers(window);
-//            glfwPollEvents();
-//            processInput(window, camera, deltaTime);
-//        }
-////[*]---------------------------------------------------------------------------------------------------------------[*]//
-//    }
-//    ImGui_ImplOpenGL3_Shutdown();
-//    ImGui_ImplGlfw_Shutdown();
-//    ImGui::DestroyContext();
-//    glfwDestroyWindow(window);
-//    glfwTerminate();
-//}
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
@@ -313,8 +175,8 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader lightingShader("res/shaders/vshader.vert", "res/shaders/fshader.frag");
-    Shader lightCubeShader("res/shaders/light.vert", "res/shaders/light.frag");
+    Shader lightingShader("../res/shaders/vshader.vert", "../res/shaders/fshader.frag");
+    Shader lightCubeShader("../res/shaders/light.vert", "../res/shaders/light.frag");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -364,7 +226,7 @@ int main()
     };
 
     OBJLoader objLoader;
-    Mesh m = objLoader.LoadFromFile("res/objs/diablo.obj");
+    Mesh m = objLoader.LoadFromFile("../res/objs/diablo.obj");
     // first, configure the cube's VAO (and VBO)
     unsigned int VBO, cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
@@ -402,7 +264,7 @@ int main()
     // load textures (we now use a utility function to keep the code more organized)
     // -----------------------------------------------------------------------------
 
-    Texture texture ("res/textures/diablo3_pose_diffuse.tga");
+    Texture texture ("../res/textures/diablo3_pose_diffuse.tga");
     glm::vec3 ambientLight(0.2f, 0.2f, 0.2f);
     glm::vec3 diffuseLight(1.0f, 0.5f, 0.5f);
 
